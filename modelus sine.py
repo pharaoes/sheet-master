e17a62926eed566b11ce2073e180282281fe083b
pharaoes <33017620+pharaoes@users.noreply.github.com>
2018-05-07 21:57:36 -0900

    Add files via upload

---
+++ modelus sine.py
@@ -0,0 +1,40 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import numpy as np
+from math import *
+
+
+def drawing_modelus_sine():
+    glClearColor(0, 0.61, 0.98, 1)
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    # draw x axis
+    glBegin(GL_LINES)
+    glVertex2f(-1.0, 0.0)
+    glVertex2f(1.0, 0.0)
+
+    # draw y axis
+    glVertex2f(0.0, -1.0)
+    glVertex2f(0.0, 1.0)
+    glEnd()
+
+    # draw the equation
+    glColor3f(0.0, 0.0, 1.0)
+    glBegin(GL_POINTS)
+    for x in np.arange(-1, 1, 0.001):
+        y = abs(sin(pi * x))
+        z = 0
+        glVertex3f(x, y, z)
+
+    glEnd()
+    glFlush()
+
+
+glutInit()
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize(600, 600)
+glutCreateWindow(b" y=|sin(PI x)|")
+glutDisplayFunc(drawing_modelus_sine)
+glutMainLoop()
+
